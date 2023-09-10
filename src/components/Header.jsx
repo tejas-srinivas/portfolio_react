8c14054ad41003a344a4c8cd225793e9c77e19c8
“tejas-srinivas” <stejas2002@gmail.com>
2023-09-10 19:50:56 +0530

    changed_header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,27 +4,40 @@ import asset from "../asset.png"
 //import useExternalScripts from '../useExternalScripts'
 
 function Header() {
-    const [theme,setTheme] = useState(localStorage.getItem('selected-theme'))
+    const [theme, setTheme] = useState(localStorage.getItem('selected-theme'))
     const themeButton = document.getElementById('theme-button')
     const darkTheme = 'dark-theme'
     const iconTheme = 'uil-sun'
+    const titles = ["Web Developer", "Logo Designer"];
+    const [title, setTitle] = useState("");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (titles[0].length > title.length) {
+                setTitle(titles[0].slice(0, title.length + 1));
+            } else {
+                setTitle("");
+            }
+        }, 500);
+        return () => clearInterval(interval);
+    }, [title]);
 
     const selectedTheme = localStorage.getItem('selected-theme')
     const selectedIcon = localStorage.getItem('selected-icon')
-    useEffect(()=>{
+    useEffect(() => {
 
-    if (selectedTheme) {
-        document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-        themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
-    }
+        if (selectedTheme) {
+            document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+            document.getElementById('theme-button').classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+        }
 
-    },[theme])
+    }, [theme])
     const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-    const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+    const getCurrentIcon = () => document.getElementById('theme-button').classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
-    const handleClick = ()=>{
+    const handleClick = () => {
         document.body.classList.toggle(darkTheme)
-        themeButton.classList.toggle(iconTheme)
+        document.getElementById('theme-button').classList.toggle(iconTheme)
 
         localStorage.setItem('selected-theme', getCurrentTheme())
         localStorage.setItem('selected-icon', getCurrentIcon())
@@ -98,7 +111,7 @@ function Header() {
                             <div className="home__data">
                                 <h2 className="home__subtitle" >Hello, I'm </h2>
                                 <h1 className="home__title" style={{ fontWeight: "700" }}>Tejas Srinivas</h1>
-                                <h3 className="home__subtitle"><span className="typed-text"></span><span className="cursor">&nbsp;</span></h3>
+                                <h3 className="home__subtitle"><span className="typed-text">{title}</span><span className="cursor">&nbsp;</span></h3>
                                 <script>
 
                                 </script>
