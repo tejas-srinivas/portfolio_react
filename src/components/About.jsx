dd027b529b79954c56f2e7e5e23261d3d9c1b392
Tejas Srinivas <109969195+tejas-srinivas@users.noreply.github.com>
2023-09-19 22:45:52 +0530

    Update Resume

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,7 +13,7 @@ function About() {
                         <p className="about__description">Greetings! 👋 I'm Tejas Srinivas, a recent Information Science graduate thrilled to kickstart a new journey in the world of technology and innovation. 
                             With a solid educational foundation and a passion for problem-solving, I'm eager to contribute my skills to the field of Information Technology.</p>
                         <div className="about__buttons">
-                            <a download="" href="https://drive.google.com/file/d/1m6eddSTDrZ30wWebI9DGj6Zy8mZRBYWe/view?usp=sharing" className="button button--flex">
+                            <a download="" href="https://drive.google.com/file/d/1Qt1K1aK95zmhBuLybjU7oDfXyNTiwSoH/view?usp=sharing" className="button button--flex">
                                 Download CV <i className="uil uil-download-alt button__icon"></i>
                             </a>
                         </div>
